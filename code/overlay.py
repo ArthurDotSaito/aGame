4a5498eebdc847e735ae23bcb8c252fb29d94196
Arthur Saito <arthur-saito@outlook.com>
2025-11-15 02:54:31 -0300

    Positions tool and seed overlay elements
    Calculates and applies positions for the tool and seed overlay elements using predefined constants, ensuring they are displayed correctly on the screen.

--- code/overlay.py
+++ code/overlay.py
@@ -12,5 +12,12 @@ class Overlay:
         self.seeds_surface = {seed:pygame.image.load(f'{overlay_path}{seed}.png').convert_alpha() for seed in player.seeds}
 
     def display(self):
+        # tools
         tool_surface = self.tools_surface[self.player.selected_tool]
-        self.display_surface.blit(tool_surface,(0,0))
+        tool_rect = tool_surface.get_rect(midbottom = OVERLAY_POSITIONS['tool'])
+        self.display_surface.blit(tool_surface, tool_rect)
+
+        # seeds
+        seed_surface = self.seeds_surface[self.player.selected_seed]
+        seed_rect = tool_surface.get_rect(midbottom=OVERLAY_POSITIONS['seed'])
+        self.display_surface.blit(seed_surface, seed_rect)
